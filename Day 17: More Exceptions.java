d65228e76210541a369c2db79a973d3dc191289a
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-02-27 20:29:03 +0530

    Update Day 17: More Exceptions.java

--- Day 17: More Exceptions.java
+++ Day 17: More Exceptions.java
@@ -16,7 +16,6 @@ class Calculator{
 class Solution{
 
     public static void main(String[] args) {
-    
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t-- > 0) {
