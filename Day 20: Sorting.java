330d0033be9cb8bb044766cd9a90081811f2ad09
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-03-04 22:21:22 +0530

    Update Day 20: Sorting.java

--- Day 20: Sorting.java
+++ Day 20: Sorting.java
@@ -4,7 +4,6 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 public class Solution {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
